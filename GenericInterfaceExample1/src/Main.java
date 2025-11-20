ff55eb3e1498d81e2014203e6471fc5dc37d5df3
Muhammet Öztürk <180087422+kaiser0o0@users.noreply.github.com>
2025-11-20 20:29:15 +0300

    refactor

--- GenericInterfaceExample1/src/Main.java
+++ GenericInterfaceExample1/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         // --- 1. KULLANICI İÇİN DEPO OLUŞTUR ---
-      
+
         IRepository<User> userRepo = new GenericDatabase<>();
 
         User ahmet = new User(1, "Ahmet");
